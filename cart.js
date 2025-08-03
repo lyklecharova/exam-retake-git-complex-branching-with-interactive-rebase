20f356eb2481b403cc9986a2f1e46b8e5908df43
lyklecharova <lklecharova9@gmail.com>
2025-08-03 09:04:58 +0300

    Add remove from cart functionality

--- cart.js
+++ cart.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-Temporaty cart logic
-=======
+
 console.log('Add product list'); 
-<<<<<<< HEAD
->>>>>>> 8663ee6 (Add product list)
-=======
 console.log('Add quantity update'); 
->>>>>>> b8a8eb3 (Implement quantity update)
+console.log('Implement quantity update')
+console.log('Remove from cart'); 
+console.log('Add remove from cart functionality')
