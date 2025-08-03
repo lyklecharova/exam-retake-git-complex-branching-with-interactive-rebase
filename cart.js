0a447142af1653e9d92f5b2a2c557cbc4d800575
lyklecharova <lklecharova9@gmail.com>
2025-08-03 09:03:03 +0300

    Add product list
    Implement quantity update

--- cart.js
+++ cart.js
@@ -1,1 +1,9 @@
+<<<<<<< HEAD
 Temporaty cart logic
+=======
+console.log('Add product list'); 
+<<<<<<< HEAD
+>>>>>>> 8663ee6 (Add product list)
+=======
+console.log('Add quantity update'); 
+>>>>>>> b8a8eb3 (Implement quantity update)
